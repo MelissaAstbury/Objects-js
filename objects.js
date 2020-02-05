34b06bfd9346ff111bb1dbbdc5b0421786cd4a9c
Melissa Astbury <melissajastbury@gmail.com>
2020-02-05 20:49:37 +0000

    learning objects - basics

---
+++ objects.js
@@ -0,0 +1,24 @@
+// let someObject = {};
+
+// someObject._name = "Hedwig";
+
+// someObject.age = 6;
+
+// console.log(someObject);
+
+// let prop = "color";
+
+// someObject[prop] = "red";
+
+// someObject.123 = true;
+
+//**********************************
+
+let someObject = {
+  friends: [{ name: "Holly", age: 9 }, { name: "Andrew" }, { name: "Sarah" }],
+  color: "baby blue",
+  isEvil: true
+};
+
+//console.log(someObject.friends[0]);
+console.log(someObject.friends[0].name);
